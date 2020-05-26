2457254f586302a322638bbc14fc25b7cfa168fc
gsergey99 <gsergey99>
2020-05-26 11:44:03 +0200

    Funcionamiento con interrupciones

--- blink_uPython/main.py
+++ blink_uPython/main.py
@@ -3,41 +3,53 @@
 from machine import Pin
 from machine import ADC
 from pyb import Timer
+from pyb import ADC
 from time import sleep
-
+import machine
 import time
-
-
-def callback_rotary(value):
-    print("Funcionando")
-
-def callback_led(value):
-    print("El valor es: ")
-
-pin_rotary = Pin('PA1',Pin.IN)
-pin_rotary.irq(handler= callback_rotary,trigger=Pin.IRQ_RISING)
-rotary_angle = ADC(pin_rotary)
-
 
 max_width = 200000
 min_width = 20000
+cur_width = 0
+
+
+
+def callback_led(value):
+    print("El valor es: ")
+    print(value)
+
+
+#DEFINIMOS ADC
+pin_rotary = pyb.Pin('PA1',pyb.Pin.IN)
+rotary_angle = pyb.ADC(pin_rotary)
+
+#DEFINIMOS EL LED
 
 pin_led = pyb.Pin('PA5', pyb.Pin.OUT)
+tim = pyb.Timer(2, freq=100) #El 2 significa que estamos utilizando una señal PMW para el LED que está asociado
+tchannel = tim.channel(1, Timer.PWM, pin=pin_led, pulse_width=0) #,callback=callback_led
 
 
-tim = pyb.Timer(2, freq=100)
-tchannel = tim.channel(1, Timer.PWM, pin=pin_led, pulse_width=0,callback=callback_led(rotary_angle))
+#DEFINIMOS EL TIMER QUE SE UTILIZARÁ PARA LA INTERRUPCIÓN
+timer_adc = pyb.Timer(5)
+timer_adc.init(freq=100)
+
+def callback_rotary(rotary_angle,channel):
+    print("Funcionando")
+    print(rotary_angle.read())
+    value_rotary = rotary_angle.read() * 16
+    channel.pulse_width(value_rotary)
+    #timer_adc.callback(callback_rotary(rotary_angle,tchannel))
+
+    
 
 print("*************LED-DIMMER*************")
-
-
+#timer_adc.init(freq=100)
+#timer_adc.callback(callback_rotary(rotary_angle,tchannel))
 
 while True:
-    rotary_value = rotary_angle.read_u16()
-    cur_width = rotary_value * 3 #Multiplicador para obtener más intensidad lumínica
-
-    tchannel.pulse_width(cur_width)
-    print(cur_width)
+    timer_adc.callback(callback_rotary(rotary_angle,tchannel))
+    
     sleep(0.01)
 
 
@@ -49,4 +61,16 @@ while True:
     #hola = pwm_led.value(intensity)
     print(intensity)
     machine.lightsleep(100)
+    #rotary_value = rotary_angle.read_u16()
+    #rotary_value = rotary_angle.read()
+    #cur_width = rotary_value * 16 #Multiplicador para obtener más intensidad lumínica
+
+    #tchannel.pulse_width(cur_width)
+    #print(cur_width)
     """
+
+"""
+pin_rotary = Pin('PA1',Pin.IN)
+pin_rotary.irq(handler= callback_rotary,trigger=Pin.IRQ_RISING)
+rotary_angle = ADC(pin_rotary)
+"""
