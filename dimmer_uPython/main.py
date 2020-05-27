81abb2a852ecaec46dce139c6bffac99a1618785
gsergey99 <gsergey99>
2020-05-27 10:56:59 +0200

    Version 1.0. Diseño de maquina de estados

--- dimmer_uPython/main.py
+++ dimmer_uPython/main.py
@@ -7,10 +7,13 @@ from pyb import ADC
 from time import sleep
 import machine
 import time
+import sys
 
 class LED_Dimmer():
     
     def __init__(self):
+        """[Definición de atributos]
+        """
         self.pin_rotary = pyb.Pin('PA1',pyb.Pin.IN)
         self.pin_led = pyb.Pin('PA5', pyb.Pin.OUT)
         self.rotary_angle = pyb.ADC(self.pin_rotary)
@@ -18,17 +21,31 @@ class LED_Dimmer():
         self.led_pwm = self.led_timer.channel(1, Timer.PWM, pin=self.pin_led, pulse_width=0)
         self.timer_adc = pyb.Timer(5,freq=100,period=99,prescaler=1,callback=self.callback_rotary)
     
-    def callback_rotary(self,value):
+    def callback_rotary(self,object):
+        """[Definición del callback de la interrupción del timer]
+
+        Arguments:
+            object -- [Objeto timer_adc por argumento]
+        """
+
         print("Funcionando")
         print(self.rotary_angle.read())
-        value_rotary = self.rotary_angle.read()*16
+        value_rotary = self.rotary_angle.read()*48
         self.led_pwm.pulse_width(value_rotary)
 
-    def iniciar(self):
+    def start(self):
+        """[Esta función inicia el timer para la captura de nuestro rotary angle sensor]
+        """
         self.timer_adc.init(freq=100,period=99,prescaler=1)
 
-
-prueba = LED_Dimmer()
-prueba.iniciar()
+print("*************LED-DIMMER*************")
+led_dimmer = LED_Dimmer()
+led_dimmer.start()
 while True:
-    sleep(0.01)
+    try:
+        sleep(0.01)
+    
+    except Exception as ex:
+        print("Any problem appears! Bye")
+        sys.exit(0)
+        
